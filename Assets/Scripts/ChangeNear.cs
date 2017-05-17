8abfb0197f358cf21511a044651257c4fbc3ef06
Victor Adriel <victorajoliveira@gmail.com>
2017-05-17 15:40:02 -0300

    Working on Oculus Rift

---
+++ Assets/Scripts/ChangeNear.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeNear : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+            Camera.current.nearClipPlane = 0.4f;
+
+    }
+}
